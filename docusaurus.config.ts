e64df5bf79810ec492ace1d98d57bf7a64ea3f9b
baegyeong <beth0929@naver.com>
2026-01-04 23:57:25 +0900

    fix: update GitHub Actions deployment configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,6 +26,7 @@ const config: Config = {
   projectName: "TIL", // Usually your repo name.
 
   onBrokenLinks: "throw",
+  trailingSlash: true,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
